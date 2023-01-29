2977b9aba07de039779242be45073a57e422ea6e
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-01-29 11:42:27 -0700

    Style info form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,45 +1,55 @@
 import Display from "./Display";
 import Table from "./Table";
+import "../styles/FormStyles.css";
 
 function Form() {
-    return (
-        <div className="Form-container">
-            <header className="Form-header">
-                <p>
-                    Determine your estimated payments for different debt amounts, interest rates & terms. 
-                    <br /><br />
-                    Start with your details.  
-                </p>
-            </header>
-            <form className="Info-form">
-                <div>
-                    <p>Total Debt Amount</p>
-                    <label htmlFor="total-debt">$</label>
-                    <input type="text" id="totalDebt" name="total-debt" required/>
-                </div>
-                <div>
-                    <p>Estimated Interest Rate</p>
-                    <input type="text" id="interestRate" name="interest-rate" />
-                    <label htmlFor="interest-rate" required>%</label>
-                </div>
-                <div>
-                    <p>Debt Payoff Term</p>
-                    <div>
-                        <input type="number" id="termYears" name="term-years" />
-                        <label htmlFor="term-years">Years</label>
-                    </div>
-                    <p>OR</p>
-                    <div>
-                        <input type="number" id="termMonths" name="term-months" />
-                        <label htmlFor="term-months">Months</label>
-                    </div>
-                </div>
-                <button className="Info-submit-btn">Calculate</button>
-            </form>
-            <Display />
-            <Table />
+  return (
+    <div className="Form-container">
+      <form className="Info-form">
+        <header className="Form-header">
+          <p>
+            Determine your estimated payments for different debt amounts,
+            interest rates & terms.
+            <br />
+            <br />
+            Start with your details.
+          </p>
+        </header>
+
+        <div className="Form-input-container">
+          <p>Total Debt Amount</p>
+          <label htmlFor="total-debt" id="totalDebtLabel">$</label>
+          <input type="text" id="totalDebt" name="total-debt" required />
         </div>
-    )
+
+        <div className="Form-input-container">
+          <p>Estimated Interest Rate</p>
+          <input type="text" id="interestRate" name="interest-rate" required />
+          <label htmlFor="interest-rate" id="interestRateLabel">
+            %
+          </label>
+        </div>
+
+        <div className="Form-input-container">
+          <p>Debt Payoff Term</p>
+          <div>
+            <input type="number" id="termYears" name="term-years" />
+            <label htmlFor="term-years" id="termYearsLabel">Years</label>
+          </div>
+          <p id="termComparison">OR</p>
+          <div>
+            <input type="number" id="termMonths" name="term-months" />
+            <label htmlFor="term-months" id="termMonthsLabel">Months</label>
+          </div>
+        </div>
+        
+        <button className="Submit-btn Info-form-btn">Calculate</button>
+      </form>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <Display />
+      <Table />
+    </div>
+  );
 }
 
 export default Form;
