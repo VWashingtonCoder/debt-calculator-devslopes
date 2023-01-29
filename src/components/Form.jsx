7218b28364fdcc681877658143fdb61022636949
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-01-29 13:47:05 -0700

    Move form logic to app.js

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,64 +1,10 @@
-import Display from "./Display";
-import Table from "./Table";
 import "../styles/FormStyles.css";
-import { useState } from "react";
 
-const infoFormData = {
-    totalDebt: '',
-    interestRate: '',
-    years: '',
-    months: ''
-}
-
-function Form() {
-    const [infoFormValues, setInfoFormValues] = useState(infoFormData);
-
-    function updateTotalDebt(evt) {
-        const inputVal = evt.target.value;
-        setInfoFormValues({ ...infoFormValues, totalDebt: inputVal });
-    } 
-
-    function updateInterestRate(evt) {
-        const inputVal = evt.target.value;
-        setInfoFormValues({ ...infoFormValues, interestRate: inputVal });
-    } 
-
-    function updateTermYears(evt) {
-        const inputVal = evt.target.value;
-        setInfoFormValues({ ...infoFormValues, years: inputVal });
-    } 
-
-    function updateTermMonths(evt) {
-        const inputVal = evt.target.value;
-        console.log(inputVal);
-        setInfoFormValues({ ...infoFormValues, months: inputVal });
-    }
-
-    function validateForm() {
-        const { totalDebt, interestRate, years, months } = infoFormValues;
-        let valid = true;
-
-        if (!totalDebt){
-            alert('You are missing the total debt');
-            valid = false;
-        } else if (!interestRate) {
-            alert('You are missing the interest rate');
-            valid = false;
-        } else if(!years && !months) {
-            alert('Pick a term length');
-            valid = false;
-        }
-
-        return valid;
-    }
-
-    function handleInfoSubmit(evt) {
-        evt.preventDefault();
-        const valid = validateForm();
-        if (valid) {
-            console.log('success!')
-        }
-    }
+function Form(props) {
+  console.log(props);
+  const { updateTotal, updateRate, updateYears, updateMonths, infoSubmit } =
+    props;
+  const { totalDebt, interestRate, years, months } = props.infoValues;
 
   return (
     <div className="Form-container">
@@ -75,24 +21,27 @@ function Form() {
 
         <div className="Form-input-container">
           <p>Total Debt Amount</p>
-          <label htmlFor="total-debt" id="totalDebtLabel">$</label>
-          <input 
-            type="number" 
-            id="totalDebt" 
-            name="total-debt" 
-            onChange={updateTotalDebt}
-            value={infoFormValues.totalDebt} 
-         />
+          <label htmlFor="total-debt" id="totalDebtLabel">
+            $
+          </label>
+          <input
+            type="number"
+            id="totalDebt"
+            name="total-debt"
+            onChange={updateTotal}
+            value={totalDebt}
+          />
         </div>
 
         <div className="Form-input-container">
           <p>Estimated Interest Rate</p>
-          <input 
-            type="number" 
-            id="interestRate" 
-            name="interest-rate" 
-            onChange={updateInterestRate} 
-        />
+          <input
+            type="number"
+            id="interestRate"
+            name="interest-rate"
+            onChange={updateRate}
+            value={interestRate}
+          />
           <label htmlFor="interest-rate" id="interestRateLabel">
             %
           </label>
@@ -101,31 +50,36 @@ function Form() {
         <div className="Form-input-container">
           <p>Debt Payoff Term</p>
           <div>
-            <input 
-                type="number" 
-                id="termYears" 
-                name="term-years"
-                onChange={updateTermYears} 
+            <input
+              type="number"
+              id="termYears"
+              name="term-years"
+              onChange={updateYears}
+              value={years}
             />
-            <label htmlFor="term-years" id="termYearsLabel">Years</label>
+            <label htmlFor="term-years" id="termYearsLabel">
+              Years
+            </label>
           </div>
           <p id="termComparison">OR</p>
           <div>
-            <input 
-                type="number" 
-                id="termMonths" 
-                name="term-months"
-                onChange={updateTermMonths} 
+            <input
+              type="number"
+              id="termMonths"
+              name="term-months"
+              onChange={updateMonths}
+              value={months}
             />
-            <label htmlFor="term-months" id="termMonthsLabel">Months</label>
+            <label htmlFor="term-months" id="termMonthsLabel">
+              Months
+            </label>
           </div>
         </div>
-        
-        <button className="Submit-btn Info-form-btn" onClick={handleInfoSubmit}>Calculate</button>
+
+        <button className="Submit-btn Info-form-btn" onClick={infoSubmit}>
+          Calculate
+        </button>
       </form>
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      <Display />
-      <Table />
     </div>
   );
 }
