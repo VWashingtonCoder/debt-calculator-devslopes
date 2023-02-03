b3ed6dc4605541a32f612e66ec8cda2864d43aa8
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-02-03 12:52:10 -0700

    Update infoForm to better match requirements

--- src/components/InfoForm.jsx
+++ src/components/InfoForm.jsx
@@ -1,65 +1,8 @@
-import { useState } from "react";
 import "../styles/FormStyles.css";
 
 function InfoForm(props) {
-  const { update } = props
-  const [infoForm, setInfoForm] = useState({
-    totalDebt: '',
-    interestRate: '',
-    years: '',
-    months: ''
-  })
-  const { totalDebt, interestRate, years, months } = infoForm;
-  /* Handlers */
-  function updateTotalDebt(evt) {
-    const inputVal = evt.target.value;
-    setInfoForm({ ...infoForm, totalDebt: inputVal });
-  }
-  function updateInterestRate(evt) {
-    const inputVal = evt.target.value;
-    setInfoForm({ ...infoForm, interestRate: inputVal });
-  }
-  function updateTermYears(evt) {
-    const inputVal = evt.target.value;
-    setInfoForm({ ...infoForm, years: inputVal });
-  }
-  function updateTermMonths(evt) {
-    const inputVal = evt.target.value;
-    setInfoForm({ ...infoForm, months: inputVal });
-  }
-  function validateForm() {
-    let valid = true;
-    
-    if (!totalDebt) {
-      alert("You are missing the total debt");
-      valid = false;
-    } else if (!interestRate) {
-      alert("You are missing the interest rate");
-      valid = false;
-    } else if (!years && !months) {
-      alert("Pick a term length");
-      valid = false;
-    } else if(years && months){
-      alert("Pick only month(s) or year(s) term");
-      valid = false;
-    }
-    return valid;
-  }
-  function handleInfoSubmit(evt) {
-    evt.preventDefault();
-    const valid = validateForm();
-    
-    if (valid) {
-      const data = {...infoForm};
-      update(data);
-    }
-  }
+  const { updateForm, updateDisplay } = props;
 
- 
- 
- 
- 
- 
   return (
     <div className="Form-container">
       <form className="Info-form">
@@ -82,9 +25,8 @@ function InfoForm(props) {
             type="number"
             id="totalDebt"
             name="total-debt"
-            onChange={updateTotalDebt}
-            value={totalDebt}
             placeholder="0"
+            onChange={updateForm}
           />
         </div>
 
@@ -95,46 +37,14 @@ function InfoForm(props) {
             id="interestRate"
             name="interest-rate"
             placeholder="0"
-            onChange={updateInterestRate}
-            value={interestRate}
+            onChange={updateForm}
           />
           <label htmlFor="interest-rate" id="interestRateLabel">
             %
           </label>
         </div>
-
-        <div className="Form-input-container">
-          <p>Debt Payoff Term</p>
-          <div>
-            <input
-              type="number"
-              id="termYears"
-              name="term-years"
-              placeholder="0"
-              onChange={updateTermYears}
-              value={years}
-            />
-            <label htmlFor="term-years" id="termYearsLabel">
-              Years
-            </label>
-          </div>
-          <p id="termComparison">OR</p>
-          <div>
-            <input
-              type="number"
-              id="termMonths"
-              name="term-months"
-              placeholder="0"
-              onChange={updateTermMonths}
-              value={months}
-            />
-            <label htmlFor="term-months" id="termMonthsLabel">
-              Months
-            </label>
-          </div>
-        </div>
-
-        <button className="Submit-btn Info-form-btn" onClick={handleInfoSubmit}>
+        
+        <button className="Submit-btn Info-form-btn" onClick={updateDisplay}>
           Calculate
         </button>
       </form>
